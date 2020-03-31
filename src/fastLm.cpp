d69516b4d73084e9874d2168ae235160db2071b9
BSchae <62944187+BSchae@users.noreply.github.com>
2020-03-31 16:57:48 +0200

    First Try

---
+++ src/fastLm.cpp
@@ -0,0 +1,18 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+List fastLm(Rcpp::NumericVector y, Rcpp::NumericMatrix x)
+{
+  int n = x.nrow(), k = x.ncol();
+  
+  arma::mat xMatrix{ x.begin(), n, k, false };
+  arma::colvec yVector{ y.begin(), y.size(), false };
+  
+  arma::colvec coef = arma::solve(xMatrix, yVector);
+  
+  return List::create(coef);
+}
+//test
