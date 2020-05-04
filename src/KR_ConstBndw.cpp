3fd72d8fcdee40366de997e4f7687db50467f026
BSchae <62944187+BSchae@users.noreply.github.com>
2020-05-04 18:45:40 +0200

    matrix Multiplication for KR Const Bndw implemented

--- src/KR_ConstBndw.cpp
+++ src/KR_ConstBndw.cpp
@@ -11,59 +11,6 @@ arma::vec kernFkt_MW320(arma::vec&, double);
 arma::vec kernFkt_MW420(arma::vec&, double);
 arma::vec kernFkt_MW422(arma::vec&, double);
 
-
-// Test
-// [[Rcpp::export]]
-arma::mat KRTest(arma::mat yMat, double h,
-  SEXP kernFcnPtr)
-{
-  int nRow{ yMat.n_rows };                // number of conditional Time-Series
-  int nCol{ yMat.n_cols };                // number of observations per Time-Series
-  int bndw{ std::max(static_cast<int>(h * nCol), 2) }; // calculate absolute bandwidth, decimals will be dumped
-  int windowWidth{ 2*bndw + 1 };          // width of estimation window
-
-  arma::mat yMatOut(nRow, nCol);          // matrix for results
-
-  //enable Kernel function
-  XPtr<funcPtr> xpfun(kernFcnPtr);
-  funcPtr kernFcn = *xpfun;
-  
-  // smoothing over boundaries
-  for (int colIndex{ 0 }; colIndex < bndw; ++colIndex)
-  {
-    double q = static_cast<double>(colIndex)/bndw;
-    arma::colvec  uBound(arma::regspace(colIndex, -bndw) / bndw);
-    arma::colvec  weightsBound{ kernFcn(uBound, q)*(1 + q)/uBound.n_rows };
-    
-    arma::mat     yLeftMat{ yMat.cols(0, uBound.n_rows - 1) };
-    arma::mat     yRightMat{ yMat.cols(nCol - uBound.n_rows, nCol - 1) };
-    
-    yMatOut.col(colIndex) = yLeftMat * weightsBound;
-    yMatOut.col(nCol - colIndex - 1) = yRightMat * reverse(weightsBound);
-    
-  }
-  return yMatOut;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //---------------------------------------------------------------------------//
 //                    Kernel Regression Functions                            //
 //---------------------------------------------------------------------------//
@@ -87,8 +34,8 @@ arma::mat KRSmooth_matrix2(arma::mat yMat, double h,
 
   // smoothing over interior values
   arma::colvec  uInterior{ arma::linspace(-bndw, bndw, windowWidth)/bndw }; //(h * nCol) };   // vector from -1 to 1 to compute weights
-  arma::colvec  weightsInterior{ kernFcn(uInterior, 1)/bndw };            // computation of weights (put in kernel-function later)
-  arma::colvec  yRowInterior{ arma::zeros(windowWidth) };                           // empty vector for use inside loop
+  arma::colvec  weightsInterior{ kernFcn(uInterior, 1)/bndw };              // computation of weights (put in kernel-function later)
+  arma::mat     yMatInterior(nRow, windowWidth);                            // empty vector for use inside loop
 
   // Loops smooth over the columns, conditional on rows. That is, every row is
   // consiedered to be an individual time series. To speed up computation, the
@@ -96,11 +43,8 @@ arma::mat KRSmooth_matrix2(arma::mat yMat, double h,
   // the weights are the same on a grid)
   for (int colIndex{ bndw }; colIndex < (nCol - bndw); ++colIndex)                  // outer loop over columns
   {
-    for (int rowIndex{ 0 }; rowIndex < nRow; ++rowIndex)                            // inner loop over rows
-    {
-      yRowInterior  = yMat(rowIndex, arma::span(colIndex - bndw, colIndex + bndw)).t();
-      yMatOut(rowIndex, colIndex) = sum(weightsInterior % yRowInterior);
-    }
+    yMatInterior = yMat.cols(colIndex - bndw, colIndex + bndw);
+    yMatOut.col(colIndex) = yMatInterior * weightsInterior;
   }
 
   // smoothing over boundaries
@@ -109,19 +53,13 @@ arma::mat KRSmooth_matrix2(arma::mat yMat, double h,
     double q = static_cast<double>(colIndex)/bndw;
     arma::colvec  uBound(arma::regspace(colIndex, -bndw) / bndw);
     arma::colvec  weightsBound{ kernFcn(uBound, q)/bndw };
-    arma::colvec  yLeft(uBound.n_rows);
-    arma::colvec  yRight(uBound.n_rows);
+    
+    arma::mat     yLeftMat{ yMat.cols(0, uBound.n_rows - 1) };
+    arma::mat     yRightMat{ yMat.cols(nCol - uBound.n_rows, nCol - 1) };
+    
+    yMatOut.col(colIndex) = yLeftMat * weightsBound;
+    yMatOut.col(nCol - colIndex - 1) = yRightMat * reverse(weightsBound);
 
-    for (int rowIndex{ 0 }; rowIndex < nRow; ++rowIndex)
-    {
-      yLeft       = weightsBound % yMat(rowIndex, arma::span(0,
-                            uBound.n_rows - 1)).t();
-      yRight      = weightsBound % reverse(yMat(rowIndex, arma::span(nCol
-                            - uBound.n_rows, nCol - 1))).t();
-
-      yMatOut(rowIndex, colIndex) = sum(yLeft);
-      yMatOut(rowIndex, nCol - colIndex - 1) = sum(yRight);
-    }
   }
   return yMatOut;
 }
